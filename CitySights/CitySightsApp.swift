9e8cacce28d08ace1c581b018abb53eb9652cd20
Leonardo Caracho <leonardo.caracho@partner.bmw.com.br>
2023-03-23 09:08:13 -0300

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Leonardo Caracho on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
